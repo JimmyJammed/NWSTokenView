9d505e9a5178ef2d1141499e9e1760fb47b73053
James Hickman <james.hickman@nitwitstudios.com>
2015-08-14 10:58:59 -0700

    Pod development

---
+++ Pod/NWSTokenView/Example/NWSTokenView/NWSTokenViewExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  NWSTokenViewExample-Bridging-Header.h
+//  NWSTokenView
+//
+//  Created by James Hickman on 8/13/15.
+//  Copyright (c) 2015 NitWit Studios. All rights reserved.
+//
+
+#ifndef NWSTokenView_NWSTokenViewExample_Bridging_Header_h
+#define NWSTokenView_NWSTokenViewExample_Bridging_Header_h
+
+#import "UIScrollView+EmptyDataSet.h"
+
+#endif
