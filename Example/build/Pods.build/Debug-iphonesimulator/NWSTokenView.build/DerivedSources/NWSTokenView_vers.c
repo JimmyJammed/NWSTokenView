d4cd63cf9625cfc20a371c4d316543412e8015b9
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-06-20 17:07:46 -0600

    Removing a bunch of files that should not have been tracked anyway

--- Example/build/Pods.build/Debug-iphonesimulator/NWSTokenView.build/DerivedSources/NWSTokenView_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char NWSTokenViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:NWSTokenView  PROJECT:Pods-1.0.0" "\n"; const double NWSTokenViewVersionNumber __attribute__ ((used)) = (double)1.0;
